9e02ce668abd30974eae0d808776cacf5910a181
UnicornTsar <92906941+UnicornTsar@users.noreply.github.com>
2024-05-29 23:49:44 +0300

    Добавил display
    По идее, должно работать, но без реализации colors я хз как правильно красить, так что доделаю потом

--- lib/isaac/canvas.rb
+++ lib/isaac/canvas.rb
@@ -11,4 +11,10 @@ class Canvas
   def fill(item)
     @pixel.fill(item)
   end
+
+  def display()
+  puts "\x1b[2J" #clears terminal
+  #puts "\x1b[48;2;200;0;0m" #sets background color
+  @matrix.each { |arr| arr.each { |x| print =begin"\x1b[38;2;2;2;2m"=end,x  }; puts  } #draws every pixel
+  end
 end
