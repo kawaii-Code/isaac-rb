8bc7d45a627566edbcdda141f45ef450225221ab
UnicornTsar <92906941+UnicornTsar@users.noreply.github.com>
2024-05-29 23:57:51 +0300

    display
    Теперь ваще должно быть

--- lib/isaac/canvas.rb
+++ lib/isaac/canvas.rb
@@ -14,7 +14,10 @@ class Canvas
 
   def display()
   puts "\x1b[2J" #clears terminal
-  #puts "\x1b[48;2;200;0;0m" #sets background color
-  @matrix.each { |arr| arr.each { |x| print =begin"\x1b[38;2;2;2;2m"=end,x  }; puts  } #draws every pixel
+  #puts "\x1b[48;2;#{@background_color[0]};#{@background_color[1]};#{@background_color[2]}m" #sets background color
+  @matrix.each_with_index do |arr, i| 
+      arr.each_with_index { |x, j| print =begin"\x1b[38;2;#{@color[i][j][0]};#{@color[i][j][1]};#{@color[i][j][2]}m"=end,x  }
+      puts  
+    end #draws every pixel
   end
 end
