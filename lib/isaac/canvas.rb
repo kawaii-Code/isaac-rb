174201a1c50fa57554dce2c09e4dad0b6710f79e
Shkobarev <danila564321@gmail.com>
2024-06-06 16:51:36 +0300

    Базовые фигуры

--- lib/isaac/canvas.rb
+++ lib/isaac/canvas.rb
@@ -40,13 +40,94 @@ module Isaac
       print "\e[H\e[?25h" # makes cursor visible
     end
 
-    def todo_get_display
-      displ = "".dup
-      @pixels.height.times do |i|
-        @pixels.width.times { |j| displ << @pixels[i, j].to_str }
-        displ << "\n"
+    def draw_rect(x, y, width, height, color="#FFFFFF", symbol='#')
+
+      if x < 0 || y < 0 || x >= @width || y >= @height
+          return
       end
-      displ
+  
+      for i in (x..x+width-1)
+  
+          if i >= @width
+              break
+          end
+  
+          @pixels[y,i] = symbol
+          #@colors[i][y] = color 
+  
+          if y+height-1 >= @height
+              next
+          end
+  
+          @pixels[y+height-1,i] = symbol
+          #@colors[i][y+height-1] = color
+  
+      end
+  
+      for j in (y..y+height-1)
+  
+          if j >= @height
+              break
+          end
+  
+          @pixels[j,x] = symbol
+          #@colors[x][j] = color 
+  
+          if x+width-1 >= @width
+              next
+          end
+  
+          @pixels[j,x+width-1] = symbol
+          #@colors[x+weight-1][j] = color
+          
+      end
+  end
+  
+  def fill_rect(x, y, width, height, color="#FFFFFF", symbol='#')
+  
+      if x < 0 || y < 0 || x >= @width || y >= @height
+          return
+      end
+  
+      for i in (x..x+width-1)
+          for j in (y..y+height-1)
+              if i >= @width || j >= @height
+                  next
+              end
+              @pixels[j,i] = symbol
+              #@colors[i][j] = color 
+          end
+      end
+  end
+
+  def draw_text (x, y, string, color="#FFFFFF", transporent_spaces=false)
+
+    if x < 0 || y < 0 || x >= @width || y >= @height
+        return
+    end
+
+    first = x
+    
+    string.each_char do |char|
+        
+      if y >= @height
+          break
+      end
+
+      if transporent_spaces && char == ' '
+          x = x+1
+      else
+          @pixels[y,x] = char
+          #@colors[x][y] = color
+          x = x+1
+      end
+      
+      if x >= @width
+          x = first
+          y = y+1
+      end
+
+      end
     end
   end
 end
