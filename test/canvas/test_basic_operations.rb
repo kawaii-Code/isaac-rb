cf2e2f561e157d1de182d5f26fed563c37a6f417
kawaii-Code <nia.personal.0@gmail.com>
2024-06-06 15:32:42 +0300

    Fixed tests

--- test/canvas/test_basic_operations.rb
+++ test/canvas/test_basic_operations.rb
@@ -4,20 +4,20 @@ describe "Basic canvas operations" do
   it "is initially empty" do
     canvas = Isaac::Canvas.new(5, 5)
 
-    display = canvas.todo_get_display
+    pixels = collect_pixels_to_string canvas
     expected = "     \n" * 5
 
-    assert_display_equals expected, display
+    assert_display_equals expected, pixels
   end
 
   it "fills the entire canvas with hashes" do
     canvas = Isaac::Canvas.new(5, 5)
     canvas.fill "#"
 
-    display = canvas.todo_get_display
+    pixels = collect_pixels_to_string canvas
     expected = "#####\n" * 5
 
-    assert_display_equals expected, display
+    assert_display_equals expected, pixels
   end
 
   it "clears the canvas" do
@@ -25,19 +25,25 @@ describe "Basic canvas operations" do
     canvas.fill "#"
     canvas.clear
 
-    display = canvas.todo_get_display
+    pixels = collect_pixels_to_string canvas
     expected = "     \n" * 5
 
-    assert_display_equals expected, display
+    assert_display_equals expected, pixels
   end
 
   it "forbids assigning a new instance of an array to pixels" do
-    assert_throws Isaac::Error do
+    canvas = Isaac::Canvas.new(5, 5)
+
+    assert_raises NoMethodError do
       canvas.pixels = %w[1 2 3]
     end
   end
 
   it "can change pixels" do
-    assert_throws
+    canvas = Isaac::Canvas.new(2, 2)
+    canvas.pixels[1, 1] = '#'
+
+    pixels = collect_pixels_to_string canvas
+    expected = "  \n #\n"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,19 @@
-# frozen_string_literal: true
-
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "isaac"
 
 require "minitest/autorun"
 
-def assert_display_equals(expected, actual)
-  (1..expected.length).each do |i|
-    assert_equal expected[i], actual[i]
+def collect_pixels_to_string(canvas)
+  string = ""
+  for row in 0..canvas.height - 1
+    for col in 0..canvas.width - 1
+      string << canvas.pixels[row, col]
+    end
+    string << "\n"
   end
+  return string
+end
+
+def assert_display_equals(expected, actual)
+  assert_equal expected, actual
 end
